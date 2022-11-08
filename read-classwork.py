f57786ff8744be18d0c42ea7ca4a9df405d738c2
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-11-08 08:44:39 -0500

    Initial files

---
+++ read-classwork.py
@@ -0,0 +1,20 @@
+# Read in the data from the "grades.json" file and save it to a variable.
+
+
+# Determine the maximum score in the grades data.
+
+
+# Count how many students achieved the maximum score in the grades data.
+
+
+# Read in the dictionary from "people.json" and save it to a variable.
+
+
+# Count how many people have True and how many people have False.
+
+
+# Were there more people with True or False? How many more?
+
+
+# What percentage of the total did people with True make up? How about
+# people with False?
---
+++ write-classwork.py
@@ -0,0 +1,22 @@
+# Create a blank list called "grades".
+
+
+# Using the random module, generate 100,000 random grades as
+# integers from 0 to 100 and add each one to the list.
+
+
+# Write the list of grades to a json file called "grades.json".
+
+
+# Create a blank dictionary called "people".
+
+
+# Read in the "names.json" file and save it to a variable.
+
+
+# Go through the names data. For each name, add it to the dictionary
+# as a key, and make its value a randomly-generated True or False.
+# Hint: Use a random number and then use if to choose True or False.
+
+
+# Write your dictionary to the "people.json" file.
